1c13a365d48f7edbf19275292cb5311384327302
carlangelodeguzman0708-rgb <carlangelodeguzman0708@gmail.com>
2026-06-17 13:44:25 +0800

    Implement local storage for game state
    Refactor game mechanics to support local storage for player moves and scores.

--- scripts/mechanics.js
+++ scripts/mechanics.js
@@ -1,15 +1,27 @@
-let move = 'o';
-let player = 'player1';
-let playerMoves = {
-  box1: '',
-  box2: '',
-  box3: '',
-  box4: '',
-  box5: '',
-  box6: '',
-  box7: '',
-  box8: '',
-  box9: '',
+let move = JSON.parse(localStorage.getItem('move1')) || 'o';
+let player = JSON.parse(localStorage.getItem('player1')) || 'player1';
+let playerMoves = JSON.parse(localStorage.getItem('playerMoves1')) || {
+  box1: '.',
+  box2: '.',
+  box3: '.',
+  box4: '.',
+  box5: '.',
+  box6: '.',
+  box7: '.',
+  box8: '.',
+  box9: '.'
+};
+
+let playerBox = {
+  box11: '',
+  box12: '',
+  box13: '',
+  box14: '',
+  box15: '',
+  box16: '',
+  box17: '',
+  box18: '',
+  box19: ''
 };
 
 let boxes = [
@@ -61,31 +73,32 @@ boxContainer.innerHTML = boxHTML;
 
 /*assigning player move just saving it in a object */
 function assigningPlayerMoves(box) {
-  if (box === 'box1' && playerMoves.box1 === '') {
+  if (box === 'box1' && playerMoves.box1 === '.') {
     playerMoves.box1 = move;
+    playerBox.box11 = 'box1'
     alternateOAndX();
-  } else if  (box === 'box2' && playerMoves.box2 === '') {
+  } else if  (box === 'box2' && playerMoves.box2 === '.') {
     playerMoves.box2 = move;
     alternateOAndX();
-  } else if (box === 'box3' && playerMoves.box3 === '') {
+  } else if (box === 'box3' && playerMoves.box3 === '.') {
     playerMoves.box3 = move;
     alternateOAndX();
-  }  else if (box === 'box4' && playerMoves.box4 === '') {
+  }  else if (box === 'box4' && playerMoves.box4 === '.') {
     playerMoves.box4 = move;
     alternateOAndX();
-  }  else if (box === 'box5' && playerMoves.box5 === '') {
+  }  else if (box === 'box5' && playerMoves.box5 === '.') {
     playerMoves.box5 = move;
     alternateOAndX();
-  }  else if (box === 'box6' && playerMoves.box6 === '') {
+  }  else if (box === 'box6' && playerMoves.box6 === '.') {
     playerMoves.box6 = move;
     alternateOAndX();
-  }  else if (box === 'box7' && playerMoves.box7 === '') {
+  }  else if (box === 'box7' && playerMoves.box7 === '.') {
     playerMoves.box7 = move;
     alternateOAndX();
-  }  else if (box === 'box8' && playerMoves.box8 === '') {
+  }  else if (box === 'box8' && playerMoves.box8 === '.') {
     playerMoves.box8 = move;
     alternateOAndX();
-  }  else if (box === 'box9' && playerMoves.box9 === '') {
+  }  else if (box === 'box9' && playerMoves.box9 === '.') {
     playerMoves.box9 = move;
     alternateOAndX();
   } 
@@ -100,72 +113,81 @@ function assigningPlayerMoves(box) {
 document.querySelector('.box1')
   .addEventListener('click', () => {
     assigningPlayerMoves('box1');
-    generatingMovesHTML('box1', move)
+    generatingMovesHTML(playerBox.box11, playerMoves.box1)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box2')
   .addEventListener('click', () => {
     assigningPlayerMoves('box2');
     generatingMovesHTML('box2', move)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box3')
   .addEventListener('click', () => {
     assigningPlayerMoves('box3');
     generatingMovesHTML('box3', move)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box4')
   .addEventListener('click', () => {
     assigningPlayerMoves('box4');
     generatingMovesHTML('box4', move)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box5')
   .addEventListener('click', () => {
     assigningPlayerMoves('box5');
     generatingMovesHTML('box5', move)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box6')
   .addEventListener('click', () => {
     assigningPlayerMoves('box6');
     generatingMovesHTML('box6', move)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box7')
   .addEventListener('click', () => {
     assigningPlayerMoves('box7');
     generatingMovesHTML('box7', move)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box8')
   .addEventListener('click', () => {
     assigningPlayerMoves('box8');
     generatingMovesHTML('box8', move)
+    saveToStorageWhenclick();
 });
 document.querySelector('.box9')
   .addEventListener('click', () => {
     assigningPlayerMoves('box9');
     generatingMovesHTML('box9', move)
+    saveToStorageWhenclick();
 });
 
 function checkWhoWins() {
-  if (playerMoves.box1 === playerMoves.box2 && playerMoves.box1 === playerMoves.box3 && playerMoves.box1 !== '') {
+  if (playerMoves.box1 === playerMoves.box2 && playerMoves.box1 === playerMoves.box3 && playerMoves.box1 !== '.') {
     triggerYouWinPopup();
     addScore();
-  } else if (playerMoves.box4 === playerMoves.box5 && playerMoves.box4 === playerMoves.box6 && playerMoves.box4 !== '') {
+  } else if (playerMoves.box4 === playerMoves.box5 && playerMoves.box4 === playerMoves.box6 && playerMoves.box4 !== '.') {
     triggerYouWinPopup();
     addScore();
-  } else if (playerMoves.box7 === playerMoves.box8 && playerMoves.box7 === playerMoves.box9 && playerMoves.box7 !== '') {
+  } else if (playerMoves.box7 === playerMoves.box8 && playerMoves.box7 === playerMoves.box9 && playerMoves.box7 !== '.') {
     triggerYouWinPopup();
     addScore();
-  } else if (playerMoves.box7 === playerMoves.box5 && playerMoves.box7 === playerMoves.box3 && playerMoves.box7 !== '') {
+  } else if (playerMoves.box7 === playerMoves.box5 && playerMoves.box7 === playerMoves.box3 && playerMoves.box7 !== '.') {
     triggerYouWinPopup();
     addScore();
-  } else if (playerMoves.box1 === playerMoves.box5 && playerMoves.box1 === playerMoves.box9 && playerMoves.box1 !== '') {
+  } else if (playerMoves.box1 === playerMoves.box5 && playerMoves.box1 === playerMoves.box9 && playerMoves.box1 !== '.') {
     triggerYouWinPopup();
     addScore();
-  } else if (playerMoves.box1 === playerMoves.box4 && playerMoves.box1 === playerMoves.box7 && playerMoves.box1 !== '') {
+  } else if (playerMoves.box1 === playerMoves.box4 && playerMoves.box1 === playerMoves.box7 && playerMoves.box1 !== '.') {
     triggerYouWinPopup();
     addScore();
-  } else if (playerMoves.box2 === playerMoves.box5 && playerMoves.box2 === playerMoves.box8 && playerMoves.box2 !== '') {
+  } else if (playerMoves.box2 === playerMoves.box5 && playerMoves.box2 === playerMoves.box8 && playerMoves.box2 !== '.') {
     triggerYouWinPopup();
     addScore();
-  } else if (playerMoves.box3 === playerMoves.box6 && playerMoves.box3 === playerMoves.box9 && playerMoves.box3 !== '') {
+  } else if (playerMoves.box3 === playerMoves.box6 && playerMoves.box3 === playerMoves.box9 && playerMoves.box3 !== '.') {
     triggerYouWinPopup();
     addScore();
   }
@@ -181,54 +203,48 @@ function alternateOAndX() {
   } 
 }
 
-function generatingMovesHTML(box, move) {
-  if (box === 'box1') {
+
+
+function generatingMovesHTML() {
     document.querySelector('.box1')
-      .innerHTML = move;
-  } else if (box === 'box2') {
+      .innerHTML = playerMoves.box1;
     document.querySelector('.box2')
-      .innerHTML = move;
-  }  else if (box === 'box3') {
+      .innerHTML = playerMoves.box2;
     document.querySelector('.box3')
-      .innerHTML = move;
-  }  else if (box === 'box4') {
+      .innerHTML = playerMoves.box3;
     document.querySelector('.box4')
-      .innerHTML = move;
-  }  else if (box === 'box5') {
+      .innerHTML = playerMoves.box4;
     document.querySelector('.box5')
-      .innerHTML = move;
-  }  else if (box === 'box6') {
+      .innerHTML = playerMoves.box5;
     document.querySelector('.box6')
-      .innerHTML = move;
-  }  else if (box === 'box7') {
+      .innerHTML = playerMoves.box6;
     document.querySelector('.box7')
-      .innerHTML = move;
-  }  else if (box === 'box8') {
+      .innerHTML = playerMoves.box7;
     document.querySelector('.box8')
-      .innerHTML = move;
-  }  else if (box === 'box9') {
+      .innerHTML = playerMoves.box8;
     document.querySelector('.box9')
-      .innerHTML = move;
-  } 
+      .innerHTML = playerMoves.box9;
   console.log(move);
   generateScoreHTML();
 }
+
 
 ////////ETO AND PARA SA YOU WIN POP UP////////
 
 function resetGame() {
   function resetObject() {
-  playerMoves.box1 = '',
-  playerMoves.box2 = '',
-  playerMoves.box3 = '',
-  playerMoves.box4 = '',
-  playerMoves.box5 = '',
-  playerMoves.box6 = '',
-  playerMoves.box7 = '',
-  playerMoves.box8 = '',
-  playerMoves.box9 = '';
+  playerMoves.box1 = '.',
+  playerMoves.box2 = '.',
+  playerMoves.box3 = '.',
+  playerMoves.box4 = '.',
+  playerMoves.box5 = '.',
+  playerMoves.box6 = '.',
+  playerMoves.box7 = '.',
+  playerMoves.box8 = '.',
+  playerMoves.box9 = '.';
   }
   resetObject();
+  
 
   function removeHTML() {
      document.querySelector('.box1')
@@ -251,6 +267,7 @@ function resetGame() {
       .innerHTML = '.';
   }
   removeHTML();
+  saveToStorageWhenclick();
 }
 
 let youWinHTML= '';
@@ -260,7 +277,7 @@ function renderYouWinHTML() {
   `
   <div class="popup-youwin">
     <div>
-    ${player} win! - ${move}
+    ${nextPlayer()} win! - ${nextMove()}
     </div>
 
     <div>
@@ -298,24 +315,40 @@ function toNextRoundHTML() {
  //   resetGame();
  // });
 
-renderYouWinHTML(player, move);
+renderYouWinHTML();
 
 /////ETO NAMAN ANG PARA SA PAG UPDATE NG SCORE////
 
-let score = {
+let score = JSON.parse(localStorage.getItem('score1')) || {
   player1: 0,
   player2: 0
 }
 
+
+
+function saveToStorageWhenclick() {
+  saveToStorage(score, 'score1');
+  saveToStorage(move, 'move1');
+  saveToStorage(playerMoves, 'playerMoves1');
+  saveToStorage(player, 'player1');
+
+
+  console.log(JSON.parse(localStorage.getItem('score1')));
+  console.log(JSON.parse(localStorage.getItem('move1')));
+  console.log(JSON.parse(localStorage.getItem('playerMoves1')));
+  console.log(JSON.parse(localStorage.getItem('player1')));
+}
+
+saveToStorageWhenclick();
+
 let scoreHTML;
-
 generateScoreHTML();
-
+generatingMovesHTML();
 function generateScoreHTML() {
   scoreHTML = `
     <div class="score-div">SCORE: ${score.player1}</div>
 
-    <div class="score-div">${nextPlayer()} Turn : Move: ${nextMove()}
+    <div class="score-div">${player} Turn : Move: ${move}
     </div>
 
     <div class="score-div">SCORE: ${score.player2}</div>
@@ -346,13 +379,30 @@ function nextPlayer() {
 }
 
 function addScore() {
-  if (player === 'player1') {
+  if (player === 'player2') {
     score.player1 += 1;
-  } else if (player === 'player2') {
+  } else if (player === 'player1') {
     score.player2 += 1;
   }
   generateScoreHTML();
 }
 
+function saveToStorage(save, name) {
+  localStorage.setItem(`${name}`, JSON.stringify(save));
+}
+
 ////////generate which move is next/////////
 
+document.querySelector('.resetscore-button')
+  .addEventListener('click', () => {
+    score.player1 = 0;  
+    score.player2 = 0;
+    generateScoreHTML();
+    saveToStorageWhenclick();
+  }) 
+document.querySelector('.resettie-button')
+  .addEventListener('click', () => {
+    resetGame();
+  }) 
+
+  
